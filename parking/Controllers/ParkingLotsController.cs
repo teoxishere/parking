0428f430edfca370b483e761e0a5d0e5cf8df866
Teox <teodor.chiretu@gmail.com>
2016-12-16 17:08:51 +0200

    Added admin section for Parking Lots.

---
+++ parking/Controllers/ParkingLotsController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using parking.Models.Domain;
+
+namespace parking.Controllers
+{
+    [Authorize]
+    public class ParkingLotsController : Controller
+    {
+        private Context db = new Context();
+
+        // GET: ParkingLots
+        public async Task<ActionResult> Index()
+        {
+            var allParkingLots = await db.ParkingLots
+                .OrderBy(pl => pl.Name)
+                .ToListAsync();
+            return View(allParkingLots);
+        }
+
+        // GET: ParkingLots/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ParkingLot parkingLot = await db.ParkingLots.FindAsync(id);
+            if (parkingLot == null)
+            {
+                return HttpNotFound();
+            }
+            return View(parkingLot);
+        }
+
+        // GET: ParkingLots/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ParkingLots/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        public async Task<ActionResult> Create(ParkingLot parkingLot)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ParkingLots.Add(parkingLot);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(parkingLot);
+        }
+
+        // GET: ParkingLots/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ParkingLot parkingLot = await db.ParkingLots.FindAsync(id);
+            if (parkingLot == null)
+            {
+                return HttpNotFound();
+            }
+            return View(parkingLot);
+        }
+
+        // POST: ParkingLots/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        public async Task<ActionResult> Edit(ParkingLot parkingLot)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(parkingLot).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(parkingLot);
+        }
+
+        // GET: ParkingLots/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ParkingLot parkingLot = await db.ParkingLots.FindAsync(id);
+            if (parkingLot == null)
+            {
+                return HttpNotFound();
+            }
+            return View(parkingLot);
+        }
+
+        // POST: ParkingLots/Delete/5
+        [HttpPost, ActionName("Delete")]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            ParkingLot parkingLot = await db.ParkingLots.FindAsync(id);
+            db.ParkingLots.Remove(parkingLot);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ parking/Scripts/MapForParkingLot.js
@@ -0,0 +1,35 @@
+﻿var map;
+
+
+function initMap() {
+    var myLatlng = { lat: 44, lng: 22 };
+    map = new google.maps.Map(document.getElementById('smallmap'), {
+        zoom: 8,
+        center: myLatlng,
+        styles: [{ "featureType": "all", "elementType": "labels", "stylers": [{ "visibility": "on" }] }, { "featureType": "all", "elementType": "labels.text.fill", "stylers": [{ "saturation": 36 }, { "color": "#000000" }, { "lightness": 40 }] }, { "featureType": "all", "elementType": "labels.text.stroke", "stylers": [{ "visibility": "on" }, { "color": "#000000" }, { "lightness": 16 }] }, { "featureType": "all", "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }, { "featureType": "administrative", "elementType": "geometry.fill", "stylers": [{ "color": "#000000" }, { "lightness": 20 }] }, { "featureType": "administrative", "elementType": "geometry.stroke", "stylers": [{ "color": "#000000" }, { "lightness": 17 }, { "weight": 1.2 }] }, { "featureType": "administrative.locality", "elementType": "labels.text.fill", "stylers": [{ "color": "#c4c4c4" }] }, { "featureType": "administrative.neighborhood", "elementType": "labels.text.fill", "stylers": [{ "color": "#707070" }] }, { "featureType": "landscape", "elementType": "geometry", "stylers": [{ "color": "#000000" }, { "lightness": 20 }] }, { "featureType": "poi", "elementType": "geometry", "stylers": [{ "color": "#000000" }, { "lightness": 21 }, { "visibility": "on" }] }, { "featureType": "poi.business", "elementType": "geometry", "stylers": [{ "visibility": "on" }] }, { "featureType": "poi.government", "elementType": "geometry.fill", "stylers": [{ "visibility": "on" }, { "color": "#ff0000" }] }, { "featureType": "poi.park", "elementType": "geometry.fill", "stylers": [{ "visibility": "off" }, { "color": "#ff0000" }] }, { "featureType": "poi.place_of_worship", "elementType": "geometry.fill", "stylers": [{ "visibility": "off" }] }, { "featureType": "road.highway", "elementType": "geometry.fill", "stylers": [{ "color": "#e31921" }, { "lightness": "0" }, { "visibility": "on" }] }, { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [{ "visibility": "off" }] }, { "featureType": "road.highway", "elementType": "labels.text.fill", "stylers": [{ "visibility": "off" }] }, { "featureType": "road.highway", "elementType": "labels.text.stroke", "stylers": [{ "visibility": "off" }, { "hue": "#ff000a" }] }, { "featureType": "road.arterial", "elementType": "geometry", "stylers": [{ "color": "#000000" }, { "lightness": 18 }] }, { "featureType": "road.arterial", "elementType": "geometry.fill", "stylers": [{ "color": "#575757" }] }, { "featureType": "road.arterial", "elementType": "labels.text.fill", "stylers": [{ "color": "#ffffff" }] }, { "featureType": "road.arterial", "elementType": "labels.text.stroke", "stylers": [{ "color": "#2c2c2c" }] }, { "featureType": "road.local", "elementType": "geometry", "stylers": [{ "color": "#000000" }, { "lightness": 16 }] }, { "featureType": "road.local", "elementType": "geometry.fill", "stylers": [{ "color": "#8e0b0b" }, { "visibility": "on" }] }, { "featureType": "road.local", "elementType": "labels.text.fill", "stylers": [{ "color": "#999999" }] }, { "featureType": "road.local", "elementType": "labels.text.stroke", "stylers": [{ "saturation": "-52" }] }, { "featureType": "transit", "elementType": "geometry", "stylers": [{ "color": "#000000" }, { "lightness": 19 }] }, { "featureType": "water", "elementType": "geometry", "stylers": [{ "color": "#000000" }, { "lightness": 17 }] }, { "featureType": "water", "elementType": "geometry.fill", "stylers": [{ "visibility": "on" }, { "color": "#ffffff" }] }]
+    });
+    
+    var marker = null;
+
+    // This event listener calls addMarker() when the map is clicked.
+    google.maps.event.addListener(map, 'click', function(e) {
+        placeMarker(e.latLng, map);
+    });
+
+    function placeMarker(position, map) {
+        if (marker != null) {
+            marker.setMap(null);
+            marker = null;
+        }
+        marker = new google.maps.Marker({
+            position: position,
+            map: map
+        });  
+        map.panTo(position);
+
+        // Also update text boxes
+        $("#Lng").val(position.lng());
+        $("#Lat").val(position.lat());
+    }
+
+}
